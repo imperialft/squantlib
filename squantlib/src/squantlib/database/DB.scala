de88ae90067d2eed0229b9570a38d201a5a22910
Hirobumi Hama <hama@yoidore.org>
2012-06-27 22:56:15 +0900

    implemented our own light-weight REPL

--- squantlib/src/squantlib/database/DB.scala
+++ squantlib/src/squantlib/database/DB.scala
@@ -7,19 +7,20 @@ import org.squeryl.Schema
 
 import squantlib.database.schemadefinitions._
 
-/**
- * DB class provides a database connection, schema definitions, and the query interface.
- * Each row in the DB corresponds to an object defined in FIXME
- * 
- * @param uri A connection string such as mysql://your.mysql.server:3128/database_name
- * @param username A username to MySQL
- * @param password Password for the user above
- *
- */
-class DB(uri:String, username:String, password:String) extends Schema {
-  Class.forName("com.mysql.jdbc.Driver")
-  SessionFactory.concreteFactory = Some(() => Session.create(DriverManager.getConnection("jdbc:" + uri, username, password), new MySQLInnoDBAdapter))
-  override def toString():String = "DB<@uri=" + uri + ">"
+object DB extends Schema {
+  
+  /**
+   * Sets up the DB connection for current thread.
+   * 
+   * @param uri A connection string such as mysql://your.mysql.server:3128/database_name
+   * @param username A username to MySQL
+   * @param password Password for the user above
+   *
+   */
+  def setup(uri:String, username:String, password:String):Unit = {
+    Class.forName("com.mysql.jdbc.Driver")
+    SessionFactory.concreteFactory = Some(() => Session.create(DriverManager.getConnection("jdbc:" + uri, username, password), new MySQLInnoDBAdapter))
+  }
 
   /**
    * Attach schema definitions to the tables.
