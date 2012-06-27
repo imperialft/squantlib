6049b899a5fb72f40bba7fcf87eefbd449476b60
Hirobumi Hama <hama@yoidore.org>
2012-06-27 20:02:55 +0900

    Reverting CLI.scala

--- squantlib/src/squantlib/database/CLI.scala
+++ squantlib/src/squantlib/database/CLI.scala
@@ -1,32 +1,33 @@
 package squantlib.database
 
 import java.io.{FileNotFoundException, FileInputStream}
+import java.net.URLClassLoader
 import java.util.Properties
 import java.lang.Thread
+import java.lang.management.{ManagementFactory, RuntimeMXBean}
 import scala.tools.nsc.interpreter._
 import scala.tools.nsc.Settings
 
 object CLI {
   val properties = new Properties
   def getProperties() = properties
- 
-  def main(args:Array[String]):Unit = {
-    if (args.length <= 0) {
-      println("Usage: scala -cp ... squantlib.database.CLI your_settings.properties")
-    } else {
-      try {
-        properties.load(new FileInputStream(args(0)))
-      } catch {
-        case e:FileNotFoundException => {
-          println("*.properties File does not exist: " + args(0))
-          System.exit(1)
-        }
-        case e:Exception => { println("error"); throw(e)}
+
+  def setup(propertiesPath:String):Unit = {
+    try {
+      properties.load(new FileInputStream(propertiesPath))
+    } catch {
+      case e:FileNotFoundException => {
+        println("*.properties File does not exist: " + propertiesPath)
+        System.exit(1)
       }
+      case e:Exception => throw(e)
     }
     setupDb()
     println("DB = " + db.toString)
-    goInteractive()
+  }
+
+  def main(args:Array[String]):Unit = {
+    goInteractive(args)
   }
 
   /**
@@ -34,27 +35,30 @@ object CLI {
    *
    */
   var db:DB = null
-  def getDb() = db
 
   /**
    * Sets up the DB connection.
    *
    */
-  private def setupDb():Unit = {
+  private def setupDb():DB = {
     db = new DB(properties.getProperty("uri"), properties.getProperty("username"), properties.getProperty("password"))
+    db
   }
 
   /**
    * Starts Scala's interactive command-line.
    *
    */
-  private def goInteractive():Unit = {
+  private def goInteractive(args:Array[String]):Unit = {
     val interpreter = new InterpreterWrapper() {
       def prompt = "squantlib> "
-//      autoRun("println(_classLoader)")
-      // Makes interpreter import these Java packages automatically.
+      for (url <- Thread.currentThread.getContextClassLoader().asInstanceOf[URLClassLoader].getURLs)
+          classPath(url.toString.replaceFirst("file:", ""))
+      // autoRun("println(System.getProperty(\"java.class.path\"))")
       // BUG: Scala 2.9.2 seems to have problem inferring a runtime classpath.
-      // autoImport("squantlib.database._")
+      autoRun("squantlib.database.CLI.setup(\"" + args(0) + "\")")
+      autoRun("cli = squantlib.database.CLI")
+      autoImport("squantlib.database._")
     }
     interpreter.startInterpreting()
   }
