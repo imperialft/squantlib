af9e8e1fd21cbd01fb97f621da3435a5f7e9126b
Katsu <masakatsu.wakayu@imperial-ft.com>
2012-06-29 16:15:42 +0900

    - Database connection adjustment - Country schema definition

--- squantlib/src/squantlib/database/schemadefinitions/SchemaDefinitions.scala
+++ squantlib/src/squantlib/database/schemadefinitions/SchemaDefinitions.scala
@@ -4,12 +4,19 @@ import java.util.Date
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.annotations.Column
 
-class Country(var id:         String,
-              @Column("currency_id")
-              var currencyId: String,
-              var name:       String,
-              @Column("short_name")
-              var shortName:  Option[String],
-              var region:     String) {
-  override def toString():String = "Country<ID=" + id + ", Name=" + name + ">"
+class Country(@Column("ID")				var id: String,
+              @Column("CurrencyID")		var currencyid: String,
+              @Column("NAME_JPN")		var name_jpn: String,
+              @Column("NAME_ENG")		var name_eng: String,
+              @Column("REGION")			var region: String,
+              @Column("DESCRIPTION_JPN")	var description_jpn: String,
+              @Column("DESCRIPTION_ENG")	var description_eng: String,
+              @Column("ADDRESS_LAT")	var address_lat: Option[Double],
+              @Column("ADDRESS_LNG")	var address_lng: Option[Double],
+              @Column("Created")	var created: Option[Date],
+              @Column("LastModified")	var lastmodified : Option[Date]
+              ) {
+  override def toString():String = "Country<ID=" + id + ", Name=" + name_eng + " " + name_jpn+ ">"
+  
+  def this() = this(null, null, null, null, null, null, null, Some(0.0), Some(0.0), Some(new Date), Some(new Date))
 }
