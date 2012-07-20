543eea048adbfda6d9fd7c63fe629f8b1147b9a8
Hirobumi Hama <hama@yoidore.org>
2012-07-20 15:19:41 +0900

    refactored CLI to work on both platforms, also adding some methods to DB

--- squantlib/src/squantlib/database/CLI.scala
+++ squantlib/src/squantlib/database/CLI.scala
@@ -1,7 +1,7 @@
 package squantlib.database
 
 import java.nio.charset.UnmappableCharacterException
-import java.io.{FileNotFoundException, FileInputStream}
+import java.io.{FileNotFoundException, FileInputStream, File}
 import java.util.Properties
 
 import org.apache.commons.lang3.StringEscapeUtils
@@ -10,71 +10,120 @@ import java.lang.management.{ManagementFactory, RuntimeMXBean}
 import scala.tools.nsc.{Interpreter, Settings}
 
 object CLI {
-  val properties = new Properties
-  def setup(propertiesPath:String):Unit = properties.load(new FileInputStream(propertiesPath))
-  def main(args:Array[String]):Unit = goInteractive(args(0))
 
   /**
-   * Starts Scala's interactive command-line.
+   * The main() expects an Array of String where the element of index 0 is *.properties file containing runtime
+   * configuration information. These below are properties currently we are using.
+   *
+   * uri      = database connection string for JDBC, such as mysql://yourmysql.com:3306/database_name
+   * username = a login user name for the database.
+   * password = the password for that login.
+   *
+   */
+  def main(args:Array[String]):Unit = {
+    setup(args(0))
+    goInteractive(args(0))
+  }
+
+  val properties = new Properties
+  def setup(propertiesPath:String):Unit = properties.load(new FileInputStream(propertiesPath))
+
+  val requiredImports = List("java.io.File",
+                                "squantlib.database._",
+                                "squantlib.database.schemadefinitions._",
+                                "squantlib.database.objectconstructor._",
+                                "org.jquantlib.time._",
+                                "org.squeryl.PrimitiveTypeMode._"
+                                )
+
+  /**
+   * @return true if the running environment is any kind of Windows.
+   */
+  def isWindows:Boolean = System.getProperty("os.name").contains("Windows")
+
+  /**
+   * @return Current working directory.
+   */
+  def currentDirectory:String = (new File("")).getAbsolutePath
+
+  /**
+   * goInteractive() starts a REPL CLI shell. At this moment, it is so hacky that I need to refactor this.
+   *
+   * @param propertiesPath A path to *.properties file that contains runtime configuration information.
    *
    */
   private def goInteractive(propertiesPath:String):Unit = {
+    val escapedPropertiesPath = StringEscapeUtils.escapeJava(propertiesPath)
     val settings = new Settings()
-    var classpath = ""
-    for (url <- Thread.currentThread.getContextClassLoader().asInstanceOf[URLClassLoader].getURLs) {
-      // HACK: The code is currently targeted to Windows.
-      val addition = url.toString.replaceFirst("file:/", "").replace("%20", " ");
-      classpath = classpath + ";" + addition;
+    var classPathBuilder = "."
+
+    if (isWindows) {
+      for (path <- Thread.currentThread.getContextClassLoader().asInstanceOf[URLClassLoader].getURLs) {
+        val cleanPath = path.toString.replaceFirst("file:/", "").replace("%20", " ")
+        classPathBuilder = classPathBuilder + ";" + cleanPath
+      }
+    } else {
+      for (path <- Thread.currentThread.getContextClassLoader().asInstanceOf[URLClassLoader].getURLs) {
+        classPathBuilder = classPathBuilder + ":" + path
+      }
     }
-    settings.classpath.value = classpath
+
+    settings.classpath.value = classPathBuilder
     val intp = new Interpreter(settings)
 
     intp beQuietDuring {
-      intp.interpret("import java.io.File")
-      intp.interpret("import squantlib.database._")
-      intp.interpret("import squantlib.database.schemadefinitions._")
-      intp.interpret("import squantlib.database.objectconstructor._")
-      intp.interpret("import org.jquantlib.time._")
-      intp.interpret("import org.squeryl.PrimitiveTypeMode._")
-      intp.interpret("CLI.setup(\"" + StringEscapeUtils.escapeJava(propertiesPath) + "\")")
-      intp.interpret("DB.setup(CLI.properties.getProperty(\"uri\"), CLI.properties.getProperty(\"username\"), CLI.properties.getProperty(\"password\"))")
-      intp.interpret("println(\"CLI's current working directory is \" + (new File(\".\").getAbsolutePath()))")
+      requiredImports.foreach(s => intp.interpret("import " + s))
+      intp.interpret("DB.setup(\"" +
+        properties.get("uri") + "\", \"" +
+        properties.get("username") + "\", \"" +
+        properties.getProperty("password") +"\")"
+      )
     }
-    
-    println("Type 'run file.scala' to run external *.scala. (Source file has to be in UTF-8)")
-    println("Type 'exit [enter]' to quit.")
 
-    // for example, this should work
-    // intp.interpret("transaction { from(DB.countries)(c => select(c)).foreach(println) }")
-    
+    printHelp()
+
+    // The REPL.
     var continue = true
+    var i = 0
     while (continue) {
       printf("squantlib> ")
-      
-      val line = readLine()
+      val line = readLine().trim
       if (line != null) {
-        if (line == "exit") {
-          continue = false
-        } else if (line.startsWith("run ")) {
-          val file = line.split(" ", 2)(1)
-          try {
-            println(file)
-            intp.interpret(scala.io.Source.fromFile(file, "UTF-8").mkString)
-          } catch {
-            case e:UnmappableCharacterException => {
-              println("Encoding issue! Please make sure the source file is in UTF-8 encoding.")
-            }
-            case e:FileNotFoundException => {
-              println("Failed to load file '" + file.replace("\\", "\\\\") + "'. Does it exist?")
-            }
+        i += 1
+        println("  [" + i +"] " + line)
+        // Exit
+        if (line == "exit") continue = false
+        else
+        // Run script
+        if (line.startsWith("run ")) {
+          val file = line.slice(4, line.length)
+          try
+            intp.interpret(getSourceContent(file))
+          catch {
+            case e: UnmappableCharacterException => println("Encoding issue found. Is it in UTF-8?")
+            case e: FileNotFoundException => println("File '" + file + "' does not exist.")
           }
-          println("")
-        } else {
-          intp.interpret(line)
-        }
-      } else {
-        continue = false
-      }
+        } else intp.interpret(line)
+      } else continue = false
     }        
   }
+
+  /**
+   * Prints help to console.
+   */
+  def printHelp() = {
+    println("Current working directory = " + currentDirectory)
+    println("Type 'run file.scala' to run external *.scala. (Source file has to be in UTF-8)")
+    println("Type 'exit [enter]' to quit.")
+  }
+
+  /**
+   * Reads entire content of the file as a String.
+   * Expect exceptions such as UnmappableCharacterException, or FileNotFoundException.
+   * @param path A path to the file.
+   * @param encoding The encoding of the file, such as UTF-8.
+   * @return Content of the file.
+   */
+  def getSourceContent(path:String, encoding:String):String = scala.io.Source.fromFile(path, encoding).mkString
+  def getSourceContent(path:String):String = getSourceContent(path, "UTF-8")
 }
