168d0af0f80b732a4cbc5576c1beaae245f6a889
Katsu <masakatsu.wakayu@imperial-ft.com>
2012-08-13 14:25:55 +0900

    batch file adjustment

--- squantlib/batch/correlation.scala
+++ squantlib/batch/correlation.scala
@@ -15,8 +15,8 @@ import squantlib.database.QLConstructors._
 import scala.collection.mutable.Map
 import java.lang.{Double => JavaDouble}
 
-class correlparams(val fx1:String, 
-				    val fx2:String, 
+class Correlparams(val name1:String, 
+				    val name2:String, 
 				    val nbDays:Int, 
 				    val datastart:Date, 
 				    val dataend:Date, 
@@ -36,30 +36,48 @@ val currencies = DB.getFXList.toSet & squantlib.model.currencies.RateConvention.
 val fxpairs = currencies.map(fx1 => currencies.map(fx2 => (fx1, fx2))).flatten
 val nbDays = 130
 
-val inputparamsfx = fxpairs.map(fx => {
-  val newparam = new correlparams(fx1 = fx._1, 
-      				fx2 = fx._2, 
+val inputparamsfx:List[Correlparams] = fxpairs.map(fx => {
+  val inputparam = new Correlparams(name1 = "FX:" + fx._1 + "JPY",
+      				name2 = "FX:" + fx._2 + "JPY", 
       				nbDays = nbDays, 
       				datastart = datastart, 
       				dataend = dataend, 
       				resultstart = resultstart, 
       				resultend = resultend)
-  newparam.ts1 = () => {println("db access") ; DB.getFXTimeSeries(newparam.datastart, newparam.dataend, fx._1).toTimeSeries}
-  newparam.ts2 = () => {println("db access") ; DB.getFXTimeSeries(newparam.datastart, newparam.dataend, fx._2).toTimeSeries}
-  newparam}
-  )
+  inputparam.ts1 = () => {println("db access") ; DB.getFXTimeSeries(inputparam.datastart, inputparam.dataend, fx._1).toTimeSeries}
+  inputparam.ts2 = () => {println("db access") ; DB.getFXTimeSeries(inputparam.datastart, inputparam.dataend, fx._2).toTimeSeries}
+  inputparam}
+  ).toList
 
-//Console.withOut(ps) {
+
+val latestparamset = DB.getPricedParamsets.maxBy(_._2)._1
+val bondids = DB.getPriceByParamset(latestparamset).map(_.bondid)
+val inputparamsbond:List[Correlparams] = bondids.map(bond => {
+  currencies.map(fx => {
+	  val inputparam = new Correlparams(name1 = "PRICE:" + bond, 
+	      				name2 = "FX:" + fx + "JPY", 
+	      				nbDays = nbDays, 
+	      				datastart = datastart, 
+	      				dataend = dataend, 
+	      				resultstart = resultstart, 
+	      				resultend = resultend)
+	  inputparam.ts1 = () => {println("db access") ; DB.getJPYPriceTimeSeries(inputparam.datastart, inputparam.dataend, bond).toTimeSeries}
+	  inputparam.ts2 = () => {println("db access") ; DB.getFXTimeSeries(inputparam.datastart, inputparam.dataend, fx).toTimeSeries}
+	  inputparam	  
+  })
+}).flatten.toList
+
+val inputparams:List[Correlparams] = inputparamsfx ++ inputparamsbond
+  
+Console.withOut(ps) {
   	val starttime = System.nanoTime
-	inputparams.par.foreach(fx => {
-//	  val ts1 = () => {println("db access") ; DB.getFXTimeSeries(fx.datastart, fx.dataend, fx.fx1).toTimeSeries}
-//	  val ts2 = () => {println("db access") ; DB.getFXTimeSeries(fx.datastart, fx.dataend, fx.fx2).toTimeSeries}
-	  CorrelPrice.price(fx.fx1, fx.ts1, fx.fx2, fx.ts2, fx.nbDays, fx.resultstart, fx.resultend)
+	inputparams.par.foreach(p => {
+	  CorrelPrice.price(p.name1, p.ts1, p.name2, p.ts2, p.nbDays, p.resultstart, p.resultend)
 	}
 	)
   	val endtime = System.nanoTime
 	println("Pricing completed: %.3f sec".format((endtime - starttime)/1000000000.0))
-//}
+}
 
 //BondPrice.pushdb
 
--- squantlib/batch/vol.scala
+++ squantlib/batch/vol.scala
@@ -8,22 +8,67 @@
 
 import java.util.{Date => JavaDate, Calendar => JavaCalendar, GregorianCalendar => JavaGCalendar, UUID}
 import org.jquantlib.time.Date
-import squantlib.task.pricing.BondPrice
+import squantlib.task.pricing.VolPrice
 import java.io.FileOutputStream
 import squantlib.database.DB
 import squantlib.database.QLConstructors._
+import scala.collection.mutable.Map
+import java.lang.{Double => JavaDouble}
 
-val ps = new java.io.FileOutputStream("log/volprice.log")
-val startdate = new Date(1, 1, 2000)
-val enddate = new Date(1, 1, 2020)
+class Volparams(val name:String, 
+				    val nbDays:Int, 
+				    val datastart:Date, 
+				    val dataend:Date, 
+				    val resultstart:Date, 
+				    val resultend:Date) {
+  
+		var ts:() => TimeSeries[JavaDouble] = null
+}
+
+val ps = new java.io.FileOutputStream("log/volatility.log")
+val resultstart = new Date(1, 1, 2001)
+val resultend = new Date(1, 1, 2020)
+val datastart = new Date(1, 1, 2000)
+val dataend = new Date(1, 1, 2020)
 val fxpairs = DB.getFXList.map(fx => (fx, "JPY"))
 val nbDays = List(65, 130, 260)
-val fxday = fxpairs.map(fx => nbDays.map(d => (fx, d))).flatten
+
+val inputparamsfx:List[Volparams] = fxpairs.map(fx => {
+  nbDays.map(d => {
+	  val inputparam = new Volparams(name = ("FX:" + fx._1 + "JPY"),
+	      				nbDays = d, 
+	      				datastart = datastart, 
+	      				dataend = dataend, 
+	      				resultstart = resultstart, 
+	      				resultend = resultend)
+	  inputparam.ts = () => {println("db access") ; DB.getFXTimeSeries(inputparam.datastart, inputparam.dataend, fx._1).toTimeSeries}
+	  inputparam
+	  })
+	}
+  ).flatten.toList
+
+val latestparamset = DB.getPricedParamsets.maxBy(_._2)._1
+val bondids = DB.getPriceByParamset(latestparamset).map(_.bondid)
+val inputparamsbond:List[Volparams] = bondids.map(bond => {
+  nbDays.map(d => {
+	  val inputparam = new Volparams(name = "PRICE:" + bond, 
+	      				nbDays = d, 
+	      				datastart = datastart, 
+	      				dataend = dataend, 
+	      				resultstart = resultstart, 
+	      				resultend = resultend)
+	  inputparam.ts = () => {println("db access") ; DB.getJPYPriceTimeSeries(inputparam.datastart, inputparam.dataend, bond).toTimeSeries}
+	  inputparam
+	})
+}).flatten.toList
+
+val inputparams:List[Volparams] = inputparamsfx ++ inputparamsbond
 
 Console.withOut(ps) {
   	val starttime = System.nanoTime
-	fxday.foreach(fxd => 
-		VolPrice.pricefx(startdate, enddate, fxd._1._1, fxd._1._2, fxd._2)
+	inputparams.par.foreach(p => {
+	  VolPrice.price(p.name, p.ts, p.nbDays, p.resultstart, p.resultend)
+	}
 	)
   	val endtime = System.nanoTime
 	println("Pricing completed: %.3f sec".format((endtime - starttime)/1000000000.0))
@@ -31,3 +76,43 @@ Console.withOut(ps) {
 
 //BondPrice.pushdb
 
+
+
+
+
+//
+///**
+// * Creates factory from given paramset.
+// * You must define following parameters in advance.
+// *  val paramset:String => parameter id
+// */
+//
+//
+//import java.util.{Date => JavaDate, Calendar => JavaCalendar, GregorianCalendar => JavaGCalendar, UUID}
+//import org.jquantlib.time.Date
+//import squantlib.task.pricing.BondPrice
+//import java.io.FileOutputStream
+//import squantlib.database.DB
+//import squantlib.database.QLConstructors._
+//
+//class FXparam (val fx1:String, val fx2:String, val nbDays:Int, val startdate:Date, val enddate:Date)
+//
+//val ps = new java.io.FileOutputStream("log/volprice.log")
+//val startdate = new Date(1, 1, 2000)
+//val enddate = new Date(1, 1, 2020)
+//val fxpairs = DB.getFXList.map(fx => (fx, "JPY"))
+//val nbDays = List(65, 130, 260)
+//val fxparams = fxpairs.map(fx => nbDays.map(d => new FXparam(fx._1, fx._2, d, startdate, enddate))).flatten
+//
+//Console.withOut(ps) {
+//  	val starttime = System.nanoTime
+//	fxparams.par.foreach(p => 
+////		VolPrice.pricefx(startdate, enddate, fxd._1._1, fxd._1._2, fxd._2)
+//		VolPrice.pricefx(p.startdate, p.enddate, p.fx1, p.fx2, p.nbDays)
+//	)
+//  	val endtime = System.nanoTime
+//	println("Pricing completed: %.3f sec".format((endtime - starttime)/1000000000.0))
+//}
+//
+////BondPrice.pushdb
+//
