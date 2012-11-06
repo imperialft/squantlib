c600a73a450f6874d02597013fbce38b289f309f
Katsu <masakatsu.wakayu@imperial-ft.com>
2012-11-06 13:52:47 +0900

    changed chart class

--- squantlib/src/squantlib/chart/LineChart.scala
+++ squantlib/src/squantlib/chart/LineChart.scala
@@ -7,48 +7,6 @@ import org.jfree.chart._
 import org.jfree.chart.plot._
 import org.jfree.data.xy._
 
-import java.awt._
-import java.awt.event._
-
-class Window(imagePath:String, width:Integer = 640, height:Integer = 480, title:String = "Untitled") extends Frame(title) {
-  setSize(width, height)
-  setVisible(true)
-  setResizable(false)
-  addWindowListener(
-    new WindowAdapter {
-      override def windowClosing(e:WindowEvent):Unit = System.exit(0)
-    }
-  )
-  override def update(g:Graphics):Unit = paint(g)
-  override def paint(g:Graphics):Unit = {
-    g.drawImage(getToolkit().getImage(imagePath), 0, 0, this)
-  }
-}
-
-abstract class AbstractChart {
-  protected def save(png:String, chart:JFreeChart, width:Integer = 640, height:Integer = 480):String = {
-    ChartUtilities.saveChartAsPNG(new File(png), chart, width, height)
-    return png
-  }
-
-  def render(png:String, data:Seq[Pair[Number, Number]], x:String = "X-axis", y:String = "Y-axis", title:String = "Untitled"):String
-
-  /**
-   * Renders a line chart onto a Window. The image rendered will be saved onto a temporary file. Probably blocks the program until the window closes.
-   *
-   * @param data Data to plot. In Seq[Pair[X, Y]] format.
-   * @param x Label for X-axis
-   * @param y Label for Y-axis
-   * @param width Image and window width
-   * @param width Image and window height
-   * @param title Title for the plot.
-   *
-   */
-  def display(data:Seq[Pair[Number, Number]], x:String = "X-axis", y:String = "Y-axis", title:String = "Untitled", width:Integer = 640, height:Integer = 480):Unit = {
-    val png = render(File.createTempFile("squantlib-", ".png").getPath(), x = x, y = y, title = title, data = data)
-    new Window(imagePath = png, width = width, height = height, title = title)
-  }
-}
 
 object LineChart extends AbstractChart {
   /**
--- squantlib/src/squantlib/chart/package.scala
+++
@@ -1,9 +0,0 @@
-package squantlib
-
-/**
- * Chart renderers and data visualizers
- * 
- */
-package object chart {
-
-}
