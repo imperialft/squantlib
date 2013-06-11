f66bf8dda51cf3c3f28c940aaa0b28c0ffcb61f9
Katsu <masakatsu.wakayu@imperial-ft.com>
2013-06-12 01:01:14 +0900

    Added manual asset

---
+++ squantlib/src/squantlib/model/asset/GenericAsset.scala
@@ -0,0 +1,15 @@
+package squantlib.model.asset
+
+import squantlib.model.StaticAsset
+import org.jquantlib.time.{Date => qlDate}
+
+case class GenericAsset(
+  override val assetID:String,
+  override val id:String,
+  override val latestPrice:Option[Double],
+  override val expectedYield:Option[Double],
+  override val expectedCoupon:Option[Double],
+  override val getDbForwardPrice:Map[qlDate, Double],
+  override val getPriceHistory:Map[qlDate, Double]
+  ) extends StaticAsset
+  
