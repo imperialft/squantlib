9347edb540c1bcb44a213094ab4c2edabadebc17
katsuwaka <masakatsu.wakayu@imperial-ft.com>
2013-11-28 17:41:04 +0900

    adjust build

--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,7 @@ object MyBuild extends Build {
    lazy val jquantlib = RootProject(uri("https://github.com/imperialft/jquantlib.git#master"))
 //   lazy val jquantlib = RootProject(file("../jquantlib"))
 
-    lazy val squantlib = Project(id = "squantlib", base = file(".")) dependsOn(jquantlib)
+    lazy val squantlib = Project(id = "squantlib", base = file(".")).dependsOn(jquantlib)
 
 
 }
