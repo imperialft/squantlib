f0015437ab32ab1ea85a91f558e81118aabb075e
katsuwaka <masakatsu.wakayu@imperial-ft.com>
2013-11-28 16:43:05 +0900

    separated jquantlib

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,9 @@ import Keys._
 
 object MyBuild extends Build {
 
-    lazy val jquantlib = Project(id = "jquantlib", base = file("jquantlib"))
+   lazy val jquantlib = RootProject(uri("https://github.com/imperialft/jquantlib.git#master"))
+//   lazy val jquantlib = RootProject(file("../jquantlib"))
+
     lazy val squantlib = Project(id = "squantlib", base = file(".")) dependsOn(jquantlib)
 
 
