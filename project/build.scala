feb3723cebc2d5d20ae610d6af6e083af176a415
katsuwaka <masakatsu.wakayu@imperial-ft.com>
2013-12-14 18:23:22 +0900

    changed build.scala

--- project/build.scala
+++ project/build.scala
@@ -3,8 +3,8 @@ import Keys._
 
 object MyBuild extends Build {
 
-   lazy val jquantlib = RootProject(uri("https://github.com/imperialft/jquantlib.git#master"))
-//   lazy val jquantlib = RootProject(file("../jquantlib"))
+// lazy val jquantlib = RootProject(uri("https://github.com/imperialft/jquantlib.git#master"))
+   lazy val jquantlib = RootProject(file("../jquantlib"))
 
     lazy val squantlib = Project(id = "squantlib", base = file(".")).dependsOn(jquantlib)
 
