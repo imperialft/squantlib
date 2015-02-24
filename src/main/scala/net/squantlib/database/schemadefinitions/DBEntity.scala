46504d073270fb35ce95cfd2dec897e2a8d2b141
katsu <masakatsu.wakayu@imperial-ft.com>
2015-02-24 14:11:33 +0900

    update db

--- src/main/scala/net/squantlib/database/schemadefinitions/DBEntity.scala
+++ src/main/scala/net/squantlib/database/schemadefinitions/DBEntity.scala
@@ -4,11 +4,14 @@ import java.sql.Timestamp
 
 trait DBEntity[T] {
   var id:T
+}
+
+trait TimestampEntity {
   var created: Timestamp
   var lastmodified: Timestamp
 }
 
-trait StringEntity extends DBEntity[String] 
+trait StringEntity extends DBEntity[String] with TimestampEntity
 
-trait IntEntity extends DBEntity[Int] 
+trait IntEntity extends DBEntity[Int] with TimestampEntity
 
