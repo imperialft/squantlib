73b75842ca705acb1209d307d5495a3a7b57beee
katsu <masakatsu.wakayu@imperial-ft.com>
2018-01-24 02:43:53 +0900

    update eqtyball

--- src/main/scala/net/squantlib/schedule/payoff/RangeForwardPayoff.scala
+++ src/main/scala/net/squantlib/schedule/payoff/RangeForwardPayoff.scala
@@ -9,6 +9,7 @@ import net.squantlib.util.FixingInformation
 import net.squantlib.util.Date
 import net.squantlib.schedule.CalculationPeriod
 import scala.reflect.ClassTag
+import org.codehaus.jackson.JsonNode
 
 
 /**
@@ -18,10 +19,9 @@ import scala.reflect.ClassTag
  * No strike is considered as no low boundary
  */
 case class RangeForwardPayoff(
-    refVariables:List[String],
-    triggerLow:List[Option[Double]],
-    triggerHigh:List[Option[Double]],
-    strikes:List[Option[Double]],
+    triggerLow:Map[String, Double],
+    triggerHigh:Map[String, Double],
+    strikes:Map[String, Double],
     var knockedIn:Boolean,
     override val physical:Boolean,
     forwardInRange:Boolean = true,
@@ -29,21 +29,18 @@ case class RangeForwardPayoff(
     description:String = null,
     inputString:String = null)(implicit val fixingInfo:FixingInformation) extends Payoff {
 
-  val variables = refVariables.toSet
+  val variables = triggerLow.keySet ++ triggerHigh.keySet ++ strikes.keySet
 
   nominal = amount
 
-  val strikeMap:Map[String, Double] = (refVariables zip strikes).collect{case (k, Some(v)) => (k, v)} (collection.breakOut)
-  val strikeVariables:Set[String] = strikeMap.keySet
+  val strikeVariables:Set[String] = strikes.keySet
+  val triggerVariables:Set[String] = triggerLow.keySet ++ triggerHigh.keySet
 
   override val isPriceable:Boolean =
-    (triggerHigh ++ triggerLow ++ strikes).forall(_ match {
-      case Some(v) => !v.isNaN && !v.isInfinity
-      case _ => true
-    }) &&
-    strikes.exists{case s => s.isDefined} &&
-    refVariables.size == triggerLow.size &&
-    refVariables.size == triggerHigh.size
+    triggerHigh.forall{case (k, v) => !v.isNaN && !v.isInfinity} &&
+    triggerLow.forall{case (k, v) => !v.isNaN && !v.isInfinity} &&
+    strikes.forall{case (k, v) => !v.isNaN && !v.isInfinity} &&
+    !strikes.isEmpty
 
   override def eventDates(period:CalculationPeriod):List[Date] = {
     if (!isPriceable) List(period.endDate)
@@ -77,28 +74,28 @@ case class RangeForwardPayoff(
       }
     }
 
-    def priceSingle(fixing:Double, isKnockedIn:Boolean):Double = {
-      if (variables.size != 1) Double.NaN
-      else if (!isKnockedIn) 1.0
-      else if (fixing.isNaN || fixing.isInfinity || variables.size != 1 || !isPriceable) Double.NaN
-      else strikes.head match {
-        case Some(v) => fixing / v
-        case _ => Double.NaN
-      }
-    }
-
-    def isKnockInSingle(fixing:Double):Boolean = {
-      if (knockedIn || variables.size != 1) true
-      else {
-        val r = (triggerLow.head, triggerHigh.head) match {
-          case (Some(l), Some(h)) => fixing >= l && fixing <= h
-          case (None, Some(h)) => fixing <= h
-          case (Some(l), None) => fixing >= l
-          case (None, None) => true
-        }
-        if (forwardInRange) r else !r
-      }
-    }
+//    def priceSingle(fixing:Double, isKnockedIn:Boolean):Double = {
+//      if (variables.size != 1) Double.NaN
+//      else if (!isKnockedIn) 1.0
+//      else if (fixing.isNaN || fixing.isInfinity || variables.size != 1 || !isPriceable) Double.NaN
+//      else strikes.head match {
+//        case Some(v) => fixing / v
+//        case _ => Double.NaN
+//      }
+//    }
+//
+//    def isKnockInSingle(fixing:Double):Boolean = {
+//      if (knockedIn || variables.size != 1) true
+//      else {
+//        val r = (triggerLow.head, triggerHigh.head) match {
+//          case (Some(l), Some(h)) => fixing >= l && fixing <= h
+//          case (None, Some(h)) => fixing <= h
+//          case (Some(l), None) => fixing >= l
+//          case (None, None) => true
+//        }
+//        if (forwardInRange) r else !r
+//      }
+//    }
 //
 //    def price(fixings:T):Double = {
 //      if (physical) {
@@ -125,34 +122,23 @@ case class RangeForwardPayoff(
 
   implicit object MapInterpreter extends FixingInterpreter[Map[String, Double]] {
 
-    def getFixings(fixings: Map[String, Double]): Option[List[Double]] = {
-      if (variables.toSet subsetOf fixings.keySet)
-        Some((0 to refVariables.size - 1).map(i => fixings(refVariables(i)))(collection.breakOut))
-      else None
-    }
+    private def validFixings(fixings:Map[String, Double], vs:Set[String]):Boolean =
+      vs.subsetOf(fixings.keySet) &&
+      vs.forall(v => !fixings(v).isNaN && !fixings(v).isInfinity) &&
+      isPriceable
 
     override def isKnockIn(fixings:Map[String, Double]):Boolean = {
       if (knockedIn) true
-      else getFixings(fixings) match {
-        case Some(fixValues) if fixValues.forall(v => !v.isNaN && !v.isInfinity) =>
-          val r = (fixValues, triggerLow, triggerHigh).zipped.forall{
-            case (f, Some(l), Some(h)) => f >= l && f <= h
-            case (f, None, Some(h)) => f <= h
-            case (f, Some(l), None) => f >= l
-            case (f, None, None) => true
-          }
-          if (forwardInRange) r else !r
-
-        case _ => true
+      else if (validFixings(fixings, triggerVariables)) {
+        val r = triggerLow.forall{case (ul, v) => fixings(ul) >= v} && triggerHigh.forall{case (ul, v) => fixings(ul) <= v}
+        if (forwardInRange) r else !r
       }
+      else true
     }
 
     def price(fixings:Map[String, Double], isKnockedIn:Boolean):Double = {
       if (!isKnockedIn) 1.0
-      else if (
-        (strikeVariables subsetOf fixings.keySet) &&
-        strikeVariables.forall(v => !fixings(v).isNaN && !fixings(v).isInfinity) &&
-        isPriceable) strikeVariables.map(v => fixings(v) / strikeMap(v)).min
+      else if (validFixings(fixings, strikeVariables)) strikeVariables.map(v => fixings(v) / strikes(v)).min
       else Double.NaN
     }
 
@@ -160,9 +146,20 @@ case class RangeForwardPayoff(
 
   implicit object DoubleInterpreter extends FixingInterpreter[Double] {
 
-    override def isKnockIn(fixing:Double):Boolean = isKnockInSingle(fixing)
+    override def isKnockIn(fixing:Double):Boolean = {
+      if (variables.size > 1) true
+      else {
+        val r = triggerLow.values.headOption.collect{case v => fixing >= v}.getOrElse(true) &&
+          triggerHigh.values.headOption.collect{case v => fixing <= v}.getOrElse(true)
+        if (forwardInRange) r else !r
+      }
+    }
 
-    override def price(fixing:Double, isKnockedIn:Boolean):Double = priceSingle(fixing, isKnockedIn)
+    override def price(fixing:Double, isKnockedIn:Boolean):Double = {
+      if (!isKnockedIn) 1.0
+      else if (variables.size != 1 || strikes.size != 1 || fixing.isNaN || fixing.isInfinity) Double.NaN
+      else fixing / strikes.values.head
+    }
   }
 
   def priceSingle[A:FixingInterpreter](fixings:A):Double = implicitly[FixingInterpreter[A]] price fixings
@@ -223,41 +220,60 @@ case class RangeForwardPayoff(
 
   override def toString =
     nominal.asPercent +
-      " [" + triggerLow.map(s => s.getOrElse("NA")).mkString(",") + "] " +
-      " [" + triggerHigh.map(s => s.getOrElse("NA")).mkString(",") + "] " +
+      " [" + triggerLow.mkString(",") + "] " +
+      " [" + triggerHigh.mkString(",") + "] " +
       nominal.asPercent +
-      " x Min([" + variables.mkString(",") + "] / [" + strikes.map(s => s.getOrElse("NA")).mkString(",") + "])"
+      " x Min([" + variables.mkString(",") + "] / [" + strikes.mkString(",") + "])"
 
 //  override def priceImpl = Double.NaN
 
-  override def jsonMapImpl = Map(
-    "type" -> "rangeforward",
-    "variable" -> variables.toArray,
-    "triggerlow" -> triggerLow.map(_.orNull).toArray,
-    "triggerhigh" -> triggerHigh.map(_.orNull).toArray,
-    "strike" -> strikes.map(_.orNull).toArray,
-    "description" -> description
-  )
+  override def jsonMapImpl = {
+    Map(
+      "type" -> "rangeforward",
+      "variable" -> (triggerLow.keySet ++ triggerHigh.keySet ++ strikes.keySet),
+      "triggerlow" -> triggerLow,
+      "triggerhigh" -> triggerHigh,
+      "strike" -> strikes,
+      "description" -> description
+    )
+  }
 
 }
 
 object RangeForwardPayoff {
 
+  def getStringOrHash(node:JsonNode, s:String, variable:List[String])(implicit fixingInfo:FixingInformation):Map[String, Double] = {
+    node.getOption(s) match {
+      case Some(n) =>
+        n.parseStringFields match {
+          case rs if !rs.isEmpty => rs.map { case (k, v) => (k, fixingInfo.updateCompute(v)) }
+            .collect { case (k, v) => (k, v.getOrElse(Double.NaN)) }
+            .toMap
+          case _ => n.parseDouble match {
+            case Some(v) if variable.size == 1 => Map(variable.head -> v)
+            case _ => Map.empty
+          }
+        }
+      case _ => Map.empty
+    }
+  }
+
   def apply(inputString:String)(implicit fixingInfo:FixingInformation):RangeForwardPayoff = {
     val formula = Payoff.updateReplacements(inputString)
     val fixed = fixingInfo.update(formula)
+    val fixedNode = fixed.jsonNode
 
     val variable:List[String] = formula.parseJsonStringList("variable").map(_.orNull)
-    
-    val triggerHigh:Option[Double] = fixed.parseJsonDouble("triggerhigh")
-    val triggerLow:Option[Double] = fixed.parseJsonDouble("triggerlow")
-    val strike:Double = fixed.parseJsonDouble("strike").getOrElse(Double.NaN)
+    val triggerHigh:Map[String, Double] = fixedNode.collect{case n => getStringOrHash(n, "triggerhigh", variable)}.getOrElse(Map.empty)
+    val triggerLow:Map[String, Double] = fixedNode.collect{case n => getStringOrHash(n, "triggerlow", variable)}.getOrElse(Map.empty)
+    val strikes:Map[String, Double] = fixedNode.collect{case n => getStringOrHash(n, "strike", variable)}.getOrElse(Map.empty)
+
     val amount:Double = fixed.parseJsonDouble("amount").getOrElse(1.0)
     val forwardInRange:Boolean = formula.parseJsonString("range_type").getOrElse("in") != "out"
     val physical:Boolean = formula.parseJsonString("physical").getOrElse("0") == "1"
     val description:String = formula.parseJsonString("description").orNull
 
-    RangeForwardPayoff(variables, triggerLow, triggerHigh, strike, false, physical, forwardInRange, amount, description, inputString)
+    RangeForwardPayoff(triggerLow, triggerHigh, strikes, false, physical, forwardInRange, amount, description, inputString)
 
   }
 
