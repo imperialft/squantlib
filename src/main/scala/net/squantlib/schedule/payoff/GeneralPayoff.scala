1efd5f3c0a08b78f4792cdac4238798d677c0ab1
katsu <masakatsu.wakayu@imperial-ft.com>
2018-09-03 16:59:06 +0900

    bug fix

--- src/main/scala/net/squantlib/schedule/payoff/GeneralPayoff.scala
+++ src/main/scala/net/squantlib/schedule/payoff/GeneralPayoff.scala
@@ -22,7 +22,7 @@ case class GeneralPayoff(
   
   override val isPriceable = formula.values.forall(v => !v.isNaN && !v.isInfinity)
   
-  override val isFixed = variables.size == 0 || super.isFixed
+  override def isFixed = variables.size == 0 || super.isFixed
   
   def leverage(index:String):Double = leverage(Set(index))
   def leverage(index:Set[String]):Double = formula.getOrElse(index, 0.0)
