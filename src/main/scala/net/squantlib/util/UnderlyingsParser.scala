14f446a74a6da689d19eb5925d77b1c2567f2928
katsu <masakatsu.wakayu@imperial-ft.com>
2019-08-08 17:42:56 +0900

    minor adjust

--- src/main/scala/net/squantlib/util/UnderlyingsParser.scala
+++ src/main/scala/net/squantlib/util/UnderlyingsParser.scala
@@ -10,24 +10,31 @@ object UnderlyingParsers {
   
   private var repository:List[UnderlyingParserGenerator[_]] = List.empty // to be initialized
   
-  def setParsers(p:List[UnderlyingParserGenerator[_]]) = repository = p
+  def setParsers(p:List[UnderlyingParserGenerator[_]]) = {
+    repository = p
+  }
   
   def getRepository = repository
   
-  def get(id:String):Option[UnderlyingParser] = 
-    if (id == null) None 
+  def get(id:String):Option[UnderlyingParser] = {
+    if (id == null) None
     else repository.map(r => r.getParser(id) match {
-        case Some(p:UnderlyingParser) => Some(p)
-        case _ => None
-        }).collect{case Some(p) => p}.headOption
+      case Some(p: UnderlyingParser) => Some(p)
+      case _ => None
+    }).collect { case Some(p) => p }.headOption
+  }
   
-  def getUnderlying(id:String, mkt:Market):Option[Underlying] = 
-    if (id == null) None else get(id).flatMap{case s => s.getUnderlying(mkt)}
+  def getUnderlying(id:String, mkt:Market):Option[Underlying] = {
+    if (id == null) None
+    else get(id).flatMap { case s => s.getUnderlying(mkt) }
+  }
   
   def isCurrency(id:String):Boolean = Currencies.keySet contains id
   
-  def extractCurrencies(id:String):Set[String] = 
-    if (id == null) Set.empty else id.grouped(3).filter(isCurrency).toSet
+  def extractCurrencies(id:String):Set[String] = {
+    if (id == null) Set.empty
+    else id.grouped(3).filter(isCurrency).toSet
+  }
   
 }
 
